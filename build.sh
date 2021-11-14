aeeae8fa3eaabae53bf086e17c6e1a59ed7b91ac
vdutradias <vinicius.dutradias@altran.com>
2021-11-14 15:08:44 +0000

    Add build script file

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git submodule update --init --recursive
+
+source poky/oe-init-build-env
+bitba
+bitbake-layers add-layer ../meta-openembedded/meta-oe/
+bitbake-layers add-layer ../meta-openembedded/meta-python/
+bitbake-layers add-layer ../meta-raspberrypi/ 
+bitbake-layers add-layer ../meta-tempmonitor/
+
+ echo "MACHINE=\"qemuarm\"">> conf/local.conf
+
+ bitbake pi-temp
