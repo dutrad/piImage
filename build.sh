0a66f28e045795d813994376ac5fcea6032a9dc6
vdutradias <vinicius.dutradias@altran.com>
2021-12-08 18:16:06 +0000

    Add missisng layers

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ source poky/oe-init-build-env
 
 bitbake-layers add-layer ../meta-openembedded/meta-oe/
 bitbake-layers add-layer ../meta-openembedded/meta-python/
+bitbake-layers add-layer ../meta-openembedded/meta-networking/
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia/
 bitbake-layers add-layer ../meta-raspberrypi/ 
 bitbake-layers add-layer ../meta-tempmonitor/
 
- echo "MACHINE=\"qemuarm\"">> conf/local.conf
+ echo "MACHINE=\"raspberrypi3\"">> conf/local.conf
 
  bitbake pi-temp
